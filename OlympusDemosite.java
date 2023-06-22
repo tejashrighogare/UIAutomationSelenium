04ee4791f9869384f46316c3e661f4642a98e698
Tejashri Ghogare <tejashrig@synerzipune.local>
2023-06-22 13:30:17 +0530

    Added POM

--- OlympusDemosite.java
+++ OlympusDemosite.java
@@ -3,30 +3,35 @@ package allTestCases;
 import static org.testng.Assert.assertEquals;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class OlympusDemosite extends WebDriverManagerBase {
+import pageObject.PageObjectForOlympus;
+
+public class OlympusDemosite extends WebDriverManagerBase{
 	private static WebDriver driver;
 	private String url;
+	PageObjectForOlympus pageObject;
 
 	@BeforeClass
 	public void setUp() {
 		driver = getBrowser("edge");
 		url = ConfigReader.getPropertyValue("baseURL");
 		driver.get(url);
+		pageObject=new PageObjectForOlympus(driver);
 	}
 
 	@Test(priority = 1, description = "Validate empty EmailId")
 	public void emptyEmailID() {
-		driver.findElement(By.id("inputWidth")).sendKeys("");
+		pageObject.setEmailid("");
 		String readPassword = ConfigReader.getPropertyValue("password");
-		driver.findElement(By.cssSelector("input[type = 'password']")).sendKeys(readPassword);
-		driver.findElement(By.cssSelector("input[type = 'submit']")).click();
+		pageObject.setPassword(readPassword);
+		pageObject.signIn();
 		String expectedUrl = driver.getCurrentUrl();
 		if (url.equals(expectedUrl)) {
-			String getEmailID = driver.findElement(By.id("inputWidth")).getText();
+			String getEmailID=pageObject.email.getText();
 			if (getEmailID.equals(""))
 				assertEquals(false, "EmailId is required.");
 		}
@@ -35,38 +40,37 @@ public class OlympusDemosite extends WebDriverManagerBase {
 	@Test(priority = 2, description = "Validate empty Password")
 	public void emptyPassword() {
 		String readEmailId = ConfigReader.getPropertyValue("emailID");
-		driver.findElement(By.id("inputWidth")).sendKeys(readEmailId);
-		driver.findElement(By.cssSelector("input[type = 'password']")).sendKeys();
-		driver.findElement(By.cssSelector("input[type = 'submit']")).click();
+		pageObject.setEmailid(readEmailId);
+		pageObject.setPassword("");
+		pageObject.signIn();
 		String expectedUrl = driver.getCurrentUrl();
 		if (url.equals(expectedUrl)) {
-			String getPassword = driver.findElement(By.cssSelector("input[type = 'password']")).getText();
+			String getPassword= pageObject.password.getText();
 			if (getPassword.equals(""))
 				assertEquals(false, "Password is required.");
 		}
 	}
 
-	@Test(priority = 3, description = "Validate empty EmailID and Password")
+@Test(priority = 3, description = "Validate empty EmailID and Password")
 	public void emptyEmailIDPassword() {
-		driver.findElement(By.id("inputWidth")).sendKeys("");
-		driver.findElement(By.cssSelector("input[type = 'password']")).sendKeys("");
-		driver.findElement(By.cssSelector("input[type = 'submit']")).click();
+		pageObject.setEmailid("");
+		pageObject.setPassword("");
+		pageObject.signIn();
 		String expectedUrl = driver.getCurrentUrl();
 		if (url.equals(expectedUrl)) {
-			String getEmailID = driver.findElement(By.id("inputWidth")).getText();
-			String getPassword = driver.findElement(By.cssSelector("input[type = 'password']")).getText();
+			String getEmailID=pageObject.email.getText();
+			String getPassword= pageObject.password.getText();
 			if (getPassword.equals("") && getEmailID.equals(""))
 				assertEquals(false, "EmailId and Password is required.");
 		}
 	}
-
-	@Test(priority = 4, description = "Validate Valid EmailID and Password")
+@Test(priority = 4, description = "Validate Valid EmailID and Password")
 	public void validEmaiIDPassword() {
 		String readEmailId = ConfigReader.getPropertyValue("emailID");
-		driver.findElement(By.id("inputWidth")).sendKeys(readEmailId);
+		pageObject.setEmailid(readEmailId);
 		String readPassword = ConfigReader.getPropertyValue("password");
-		driver.findElement(By.cssSelector("input[type = 'password']")).sendKeys(readPassword);
-		driver.findElement(By.cssSelector("input[type = 'submit']")).click();
+		pageObject.setPassword(readPassword);
+		pageObject.signIn();
 		String expectedUrl = "https://www.olympus-ims.com/en/login/?";
 		String actualUrl = driver.getCurrentUrl();
 		Assert.assertEquals(actualUrl, expectedUrl);
