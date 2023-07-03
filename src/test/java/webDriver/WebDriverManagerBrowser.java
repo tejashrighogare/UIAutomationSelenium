75579911ca4bd53092983c0b84c9bd1ac7f118e7
Tejashri Ghogare <tejashrig@synerzipune.local>
2023-07-03 11:39:35 +0530

    added

---
+++ src/test/java/webDriver/WebDriverManagerBrowser.java
@@ -0,0 +1,35 @@
+package webDriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class WebDriverManagerBrowser {
+	public WebDriver getBrowser(String browserType) {
+		WebDriver driver;
+		switch (browserType) {
+		case "firefox":
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+			driver.manage().window().maximize();
+			break;
+		case "chrome":
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+			driver.manage().window().maximize();
+			break;
+		case "edge":
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+			driver.manage().window().maximize();
+			break;
+		default:
+			System.out.println("browser : " + browserType + " is invalid, Launching Chrome as browser of choice..");
+			driver = new ChromeDriver();
+			driver.manage().window().maximize();
+		}
+		return driver;
+	}
+}
