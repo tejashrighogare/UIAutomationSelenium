91a200ea31d93835733d81648a5a0d0fb37d8524
Tejashri Ghogare <tejashrig@synerzipune.local>
2023-07-04 14:33:55 +0530

    done

--- src/test/java/pageObject/PageObjectForOlympus.java
+++ src/test/java/pageObject/PageObjectForOlympus.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class PageObjectForOlympus {
-	WebDriver localDriver;
+	private WebDriver localDriver;
 
 	public PageObjectForOlympus(WebDriver remoteDriver) {
 		localDriver = remoteDriver;
